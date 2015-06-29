6d9b6c87b2cd227e185d8b07fc26e5611c9aaa85
BinaryMoon <BinaryMoon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 11:12:05 +0000

    Make the images work properly on secure domains
    git-svn-id: https://plugins.svn.wordpress.org/browser-shots/trunk@1189475 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- browser-shots.php
+++ browser-shots.php
@@ -4,7 +4,7 @@ Plugin Name: Browser Shots
 Plugin URI: https://wordpress.org/plugins/browser-shots/
 Description: Easily take dynamic screenshots of a website inside of WordPress
 Author: Kevin Leary
-Version: 1.3.2
+Version: 1.4
 Author URI: http://prothemedesign.com
 */
 
@@ -24,8 +24,10 @@ class BrowserShots {
 	 * @staticvar
 	 */
 	function __construct( $options = array() ) {
+
 		add_shortcode( 'browser-shot', array( $this, 'shortcode' ) );
 		add_action( 'init', array( $this, 'tinymce_button' ) );
+
 	}
 
 
@@ -76,7 +78,7 @@ class BrowserShots {
 ?>
 	<div class="browser-shot">
 		<a href="<?php echo $link; ?>" <?php echo $target; ?>>
-			<img src="<?php echo $image_uri; ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" class="alignnone" />
+			<img src="<?php echo esc_url( $image_uri ); ?>" alt="<?php echo $alt; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" class="alignnone" />
 		</a>
 	</div>
 <?php
@@ -85,10 +87,13 @@ class BrowserShots {
 				echo '<p class="wp-caption-text">' . $caption . '</p>';
 				echo '</div>';
 			}
+
 			return ob_get_clean();
+
 		}
 
 		return '';
+
 	}
 
 
@@ -100,14 +105,22 @@ class BrowserShots {
 	public function get_shot( $url = '', $width = 600, $height = 450 ) {
 
 		// Image found
-		if ( $url != '' ) {
-			return 'http://s.wordpress.com/mshots/v1/' . urlencode( $url ) . '?w=' . $width . '&h=' . $height;
+		if ( '' != $url ) {
+
+			$query_args = array(
+				'w' => intval( $width ),
+				'h' => intval( $height ),
+			);
+
+			return add_query_arg( $query_args, 'https://s0.wordpress.com/mshots/v1/' . urlencode( $url ) );
+
+		} else {
+
+			// No image
+			return '';
+
 		}
 
-		// No image
-		else {
-			return '';
-		}
 	}
 
 
@@ -115,9 +128,11 @@ class BrowserShots {
 	 * Register TinyMCE Button
 	 */
 	public function register_button( $buttons ) {
+
 		array_push( $buttons, "|", "browsershots" );
 
 		return $buttons;
+
 	}
 
 
@@ -125,9 +140,11 @@ class BrowserShots {
 	 * Register TinyMCE Plugin
 	 */
 	public function add_plugin( $plugin_array ) {
+
 		$plugin_array['browsershots'] = plugins_url( 'js/browser-shots.js' , __FILE__ );
 
 		return $plugin_array;
+
 	}
 
 
@@ -137,14 +154,21 @@ class BrowserShots {
 	public function tinymce_button() {
 
 		// Capabilities check
-		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) )
+		if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) {
+
 			return;
 
+		}
+
 		if ( get_user_option( 'rich_editing' ) == 'true' ) {
+
 			add_filter( 'mce_external_plugins', array( $this, 'add_plugin' ) );
 			add_filter( 'mce_buttons', array( $this, 'register_button' ) );
+
 		}
+
 	}
+
 }
 
 new BrowserShots();
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: BinaryMoon, kevinlearynet
 Tags: screenshot, browser, browser shot, generator, tool, automatic, shortcode, automate, screenshots, shots, web browser, window, snap, website, website screenshot, website preview
 Requires at least: 3.6
 Tested up to: 4.2
-Stable tag: 1.3.2
+Stable tag: 1.4
 
 Automate the process of taking website screenshots.
 
@@ -46,6 +46,10 @@ The *Browser Shots* plugin is compatibly with WordPress Multisite, just use the 
 2. The formatted front-end view of generated screenshots. [See a live example](http://www.kevinleary.net/great-examples-software-web-design/)
 
 == Changelog ==
+
+= 1.4 =
+* Update the website screen capture path. This ensures the code will work properly on secure domains
+* Add some additional value escaping for extra security
 
 = 1.3.2 =
 * Fix a couple of small javascript bugs (thanks again to Ciprian Dracea)
